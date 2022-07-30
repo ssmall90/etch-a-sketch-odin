f38a041b27aa29b23ac893e9c99eaee8e8a49ef6
Sheldon Small <ssmall9595@icloud.com>
2022-07-30 16:29:17 +0100

    Following edits made ; created button which prompts the user to select a grid size grid size loads according to user selection previous grid is now replaced after each button click click listener added - unhighlights squares highlighted by the user

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container')
 const gridButton = document.querySelector('.grid-button')
 
-let gridSize =  4
+let gridSize = 5
 
 const createGrid = (gridSize) => {
     for (let i = 0; i < gridSize; i++) {
@@ -10,14 +10,29 @@ const createGrid = (gridSize) => {
 
 
      for (let j = 0; j < gridSize; j++) {
+        const flexSize = 500 / gridSize
          const column = document.createElement('div')
          column.classList.add('column-row')
          column.addEventListener('mouseenter',() =>
-         column.style.backgroundColor = 'black')
+         column.style.backgroundColor = 'blue')
+         column.style.width = `${flexSize}px`
+         column.style.height = `${flexSize}px`
          row.appendChild(column)
+         column.addEventListener('click', () => 
+         column.style.backgroundColor = 'white'
+         )
      }   
      container.appendChild(row)
     }
 }
+
+gridButton.addEventListener('click', () => {
+    let userSize = Number(prompt('choose the dimension of your grid'))
+    while (userSize > 100){
+        userSize = Number(prompt('Choose a number smaller than 100'))
+    }
+    container.replaceChildren()
+    createGrid(userSize)
+})
 
 createGrid(gridSize)
